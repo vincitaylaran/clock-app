cc9c907a6a1c979941161a390946d92b94f0e93c
Vinci Taylaran <svl.taylaran@gmail.com>
2021-01-19 14:48:11 -0700

    Create <Quote />

---
+++ src/components/Quote.tsx
@@ -0,0 +1,12 @@
+function Quote() {
+  return (
+    <h4>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente placeat
+      praesentium neque! Blanditiis explicabo hic eum illum porro officia
+      accusantium, aliquid, voluptatibus non qui voluptas nesciunt eius modi
+      neque perspiciatis.
+    </h4>
+  )
+}
+
+export default Quote
