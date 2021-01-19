50f15cbef53fc1cd0498ed3ebfc544a83182bbbc
Vinci Taylaran <svl.taylaran@gmail.com>
2021-01-19 14:48:57 -0700

    Create styles for <ExpandButton />

--- src/components/ExpandButton.tsx
+++ src/components/ExpandButton.tsx
@@ -1,5 +1,11 @@
+import "styles/_expand-button.scss"
+
 function ExpandButton() {
-  return <button className="time__more-button">More</button>
+  return (
+    <div className="button-container">
+      <button className="button-container__more-button">More</button>
+    </div>
+  )
 }
 
 export default ExpandButton
