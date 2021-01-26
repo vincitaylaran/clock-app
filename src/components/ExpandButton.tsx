0592b0351182b126fb5ba50cbb1579b8ef390f24
Vinci Taylaran <svl.taylaran@gmail.com>
2021-01-26 13:40:41 -0700

    Create styles for <ExpandButton />

--- src/components/ExpandButton.tsx
+++ src/components/ExpandButton.tsx
@@ -5,18 +5,34 @@ interface Props {
   isMoreClicked: boolean
 }
 
-function ExpandButton({ onMore, isMoreClicked }: Props) {
-  // const { onMore } = useMoreInfo()
+function ArrowIcon({ isMoreClicked }: { isMoreClicked: boolean }) {
+  return (
+    <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fill-rule="evenodd">
+        <circle id="circle-icon" fill="#303030" cx="20" cy="20" r="17" />
+        <path
+          className={`${isMoreClicked ? "" : "flipped"}`}
+          id="arrow-icon"
+          stroke="#FFF"
+          stroke-width="2"
+          d="M14 23l6-6 6 6"
+        />
+      </g>
+    </svg>
+  )
+}
 
+function ExpandButton({ onMore, isMoreClicked }: Props) {
   const handleOnMore = () => {
     onMore(isMoreClicked ? false : true)
   }
 
   return (
-    <div>
-      <button onClick={handleOnMore} className="more-button">
-        More
-      </button>
+    <div onClick={handleOnMore} className="more-button">
+      <span className="more-button__text">
+        {isMoreClicked ? "less" : "more"}
+      </span>
+      <ArrowIcon isMoreClicked={isMoreClicked} />
     </div>
   )
 }
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,3 +1,5 @@
+import "styles/_quote-and-time.scss"
+
 export interface RandomQuote {
   content: string | undefined
   author: string | undefined
@@ -9,10 +11,12 @@ interface Props {
   onNewQuote: () => void
 }
 
-function SVGRefresh() {
+// TODO: change opacity of icon on hover
+function RefreshIcon() {
   return (
     <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
       <path
+        id="refresh-icon"
         d="M7.188 10.667a.208.208 0 01.147.355l-2.344 2.206a5.826 5.826 0 009.578-2.488l2.387.746A8.322 8.322 0 013.17 14.94l-2.149 2.022a.208.208 0 01-.355-.148v-6.148h6.52zm7.617-7.63L16.978.958a.208.208 0 01.355.146v6.23h-6.498a.208.208 0 01-.147-.356L13 4.765A5.825 5.825 0 003.43 7.26l-2.386-.746a8.32 8.32 0 0113.76-3.477z"
         fill="#FFF"
         fillRule="nonzero"
@@ -28,7 +32,7 @@ function Quote({ quote, onNewQuote, isMoreClicked }: Props) {
       <div id="quote">
         <h5 id="content">"{quote.content}" </h5>
         <button className="refresh-button" onClick={onNewQuote}>
-          <SVGRefresh />
+          <RefreshIcon />
         </button>
       </div>
       <h4 id="author">{quote.author}</h4>
